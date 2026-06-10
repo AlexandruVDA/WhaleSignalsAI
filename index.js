fda191313a65856ab85bf91ad5e6e81c4367fd17
AlexandruVDA <80089108+AlexandruVDA@users.noreply.github.com>
2026-06-10 10:47:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const axios = require("axios");
 const fs = require("fs");
 const { ethers } = require("ethers");
 const sharp = require("sharp");
-try {
-  GlobalFonts.registerFromPath("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", "Arial");
-  GlobalFonts.registerFromPath("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", "Arial");
-  console.log("Fonts loaded");
-} catch (err) {
-  console.log("Font load failed:", err.message);
-}
+
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
